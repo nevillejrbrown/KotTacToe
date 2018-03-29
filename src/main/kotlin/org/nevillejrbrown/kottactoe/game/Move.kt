2829fe9f58372204a56798ace03ad7e9a1c1e6b0
Neville Brown <neville.brown@gmail.com>
2018-03-29 20:29:12 +0100

    Changed to be a data class

--- src/main/kotlin/org/nevillejrbrown/kottactoe/game/Move.kt
+++ src/main/kotlin/org/nevillejrbrown/kottactoe/game/Move.kt
@@ -7,22 +7,6 @@ package org.nevillejrbrown.kottactoe.game
 | | | |
 |O| | |
 --------
-
-
  **/
 
-class Move(val row: Int, val col: Int, val mark: Mark) {
-
-    override fun toString(): String {
-        return "$row:$col,${mark.toString()}"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (other is Move) {
-            return ((this.row == other.row) &&
-                    (this.col == other.col) &&
-                    (this.mark == other.mark)
-                    )
-        } else return false
-    }
-}
+data class Move(val row: Int, val col: Int, val mark: Mark)
